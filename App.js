ed1d29c4d0c47835e4f8136fea33b84006e0cceb
Richard Lin <rlin7@student.gsu.edu>
2020-08-14 22:19:11 -0400

    working, pre-recursion

--- App.js
+++ App.js
@@ -22,6 +22,7 @@ export default class App extends Component {
       showTimer,
       showBreakTimer,
       stopTime,
+      stopBreak,
     } = this.state;
 
     if (finishedGoal === false && showBreakTimer === false) {
@@ -29,7 +30,7 @@ export default class App extends Component {
         <View style={styles.container}>
           {showTimer && (
             <CountdownCircle
-              seconds={2}
+              seconds={3}
               radius={30}
               borderWidth={8}
               color="green"
@@ -67,7 +68,6 @@ export default class App extends Component {
               />
             </View>
           )}
-
           <StatusBar style="auto" />
         </View>
       );
@@ -76,21 +76,51 @@ export default class App extends Component {
     if (showBreakTimer === true && finishedGoal === false) {
       return (
         <View style={styles.container}>
-          <CountdownCircle
-            seconds={10}
-            radius={30}
-            borderWidth={8}
-            useNativeDriver={true}
-            color="#ff003f"
-            bgColor="#fff"
-            textStyle={{ fontSize: 20 }}
-            onTimeElapsed={this._8Timer}
-          />
-          <Text></Text>
-          <Text> Main Goal: {this.state.mGoal}</Text>
-          <Text> Intermediate Goal: {this.state.iGoal}</Text>
-          <Text></Text>
+          {stopBreak === false && (
+            <View style={styles.container}>
+              <CountdownCircle
+                seconds={10}
+                radius={30}
+                borderWidth={8}
+                useNativeDriver={true}
+                color="#ff003f"
+                bgColor="#fff"
+                textStyle={{ fontSize: 20 }}
+                onTimeElapsed={this._8Timer}
+              />
+              <Text></Text>
+              <Text> Main Goal: {this.state.mGoal}</Text>
+              <Text> Intermediate Goal: {this.state.iGoal}</Text>
+              <Text></Text>
+              <Question
+                showInt={() => this.hideComponent("showInt2")}
+                showTimer={() => this.hideComponent("showTimer")}
+                isFinished={this._isFinished}
+                breakOver={this._breakOver}
+                onClick={() => this.hideComponent("showInt")}
+                iGoal={this.state.iGoal}
+                onChange={this._intChange}
+              />
+              <StatusBar style="auto" />
+            </View>
+          )}
 
+          {stopBreak && (
+            <View>
+              <Text>Times Up! Click to Continue...</Text>
+              <Button onPress={this._breakOver} title={"Continue"} />
+              <StatusBar style="auto" />
+            </View>
+          )}
+          <StatusBar style="auto" />
+        </View>
+      );
+    }
+
+    if (finishedGoal === true) {
+      return (
+        <View style={styles.container}>
+          <Text>Good Job! Enjoy the rest of your day!</Text>
           <StatusBar style="auto" />
         </View>
       );
@@ -109,15 +139,18 @@ export default class App extends Component {
       break: true,
 
       stopTime: false,
+      stopBreak: false,
 
       mGoal: "",
       iGoal: "",
     };
-
+    this._isFinished = this._isFinished.bind(this);
+    this._showQuestion = this._showQuestion.bind(this);
     this._mainChange = this._mainChange.bind(this);
     this._intChange = this._intChange.bind(this);
+    this._breakOver = this._breakOver.bind(this);
     this._22Timer = this._22Timer.bind(this);
-    this._showQuestion = this._showQuestion.bind(this);
+    this._8Timer = this._8Timer.bind(this);
   }
 
   _mainChange(event) {
@@ -131,10 +164,23 @@ export default class App extends Component {
   _showQuestion() {
     this.audioPlayer.unloadAsync();
     this.setState({
-      showQA: true,
       showTimer: false,
       showBreakTimer: true,
+      stopTime: false,
     });
+  }
+
+  _breakOver() {
+    this.audioPlayer.unloadAsync();
+    this.setState({
+      showBreakTimer: false,
+      showTimer: true,
+      stopBreak: false,
+    });
+  }
+
+  _isFinished() {
+    this.setState({ finishedGoal: true });
   }
 
   playSound = async () => {
@@ -148,11 +194,13 @@ export default class App extends Component {
 
   _22Timer() {
     this.playSound();
-
     this.setState({ showTimer: false, stopTime: true });
   }
 
-  _8Timer = () => {};
+  _8Timer() {
+    this.playSound();
+    this.setState({ stopBreak: true });
+  }
 
   hideComponent(name) {
     switch (name) {
@@ -166,15 +214,138 @@ export default class App extends Component {
         break;
       case "showQ&A":
         this.setState({ showTimer: !this.state.showTimer });
-        this.setState({ showQA: !this.state.showQA });
+
         break;
       case "showInt2":
         this.setState({ showInt: !this.state.showInt });
-        this.setState({ showQA: !this.state.showQA });
+
         break;
       case "showTimer":
-        this.setState({ showQA: !this.state.showQA });
         this.setState({ showTimer: !this.state.showTimer });
+        break;
+      default:
+        return;
+    }
+  }
+}
+
+class Question extends React.Component {
+  render() {
+    const { showQuestion, showMet, showDone, newGoal, breakTime } = this.state;
+
+    if (showQuestion) {
+      return (
+        <View>
+          <Text></Text>
+          <Text>Was Intermediate Goal relevant?</Text>
+          <Text></Text>
+          <Button onPress={() => this.hideComponent("rGoal")} title={"Yes"} />
+          <Button onPress={() => this.hideComponent("newGoal")} title={"No"} />
+        </View>
+      );
+    }
+
+    if (showMet) {
+      return (
+        <View>
+          <Text></Text>
+          <Text>Has Intermediate Goal been met?</Text>
+          <Text></Text>
+          <Button onPress={() => this.hideComponent("met")} title={"Yes"} />
+          <Button onPress={() => this.hideComponent("break")} title={"No"} />
+        </View>
+      );
+    }
+
+    if (showDone) {
+      return (
+        <View>
+          <Text></Text>
+          <Text>
+            Would you like to set a new Intermediate Goal or are you done with
+            the Main Goal?
+          </Text>
+          <Text></Text>
+          <Button
+            onPress={() => this.hideComponent("newGoal")}
+            title={"New Intermediate Goal"}
+          />
+          <Button
+            onPress={this.props.isFinished}
+            title={"Done with Main Goal."}
+          />
+        </View>
+      );
+    }
+
+    if (newGoal) {
+      return (
+        <View>
+          <Text>Please tell me your Intermediate Goal: </Text>
+          <TextInput
+            style={{ borderColor: "gray", borderWidth: 1 }}
+            type="text"
+            placeholder="Type here!"
+            value={this.props.iGoal}
+            onChangeText={this.props.onChange}
+          />
+          <Button
+            onPress={() => this.hideComponent("break")}
+            title={"Submit"}
+          />
+        </View>
+      );
+    }
+
+    if (breakTime) {
+      return (
+        <View>
+          <Text>Take a break for the remaining time.</Text>
+        </View>
+      );
+    }
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      showQuestion: true,
+      showMet: false,
+      showDone: false,
+      newGoal: false,
+      BreakTime: false,
+    };
+  }
+
+  hideComponent(name) {
+    switch (name) {
+      case "rGoal":
+        this.setState({
+          showQuestion: !this.state.showQuestion,
+          showMet: !this.state.showMet,
+        });
+        break;
+      case "met":
+        this.setState({
+          showMet: !this.state.showMet,
+          showDone: !this.state.showDone,
+        });
+        break;
+      case "newGoal":
+        this.setState({
+          showQuestion: false,
+          showMet: false,
+          showDone: false,
+          newGoal: true,
+        });
+        break;
+      case "break":
+        this.setState({
+          showMet: false,
+          showDone: false,
+          newGoal: false,
+          breakTime: true,
+        });
         break;
       default:
         return;
